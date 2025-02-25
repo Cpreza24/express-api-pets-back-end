1e9269ddaf0b64a45067ffab06648027fd85fc48
Cesar Preza <cpreza93@yahoo.com>
2025-02-24 19:12:57 -0800

    adds pet show route

--- controllers/pets.js
+++ controllers/pets.js
@@ -13,12 +13,23 @@ router.post('/', async (req, res) => {
   }
 });
 
+//INDEX - Get ALL items in the pets array
 router.get('/', async (req, res) => {
   try {
     const foundPets = await Pet.find();
     res.status(200).json(foundPets);
   } catch (error) {
     res.status(500).json({ err: err.message });
+  }
+});
+
+// SHOW - Show 1 item based off the id
+router.get('/:petId', async (req, res) => {
+  try {
+    const foundPet = await Pet.findById(req.params.petId);
+    res.status(200).json(foundPet);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
