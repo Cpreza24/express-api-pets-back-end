a1e4a05454054946fe3fae94eedcc43725f9b473
Cesar Preza <cpreza93@yahoo.com>
2025-02-24 20:02:54 -0800

    adds update pet route

--- controllers/pets.js
+++ controllers/pets.js
@@ -41,7 +41,7 @@ router.get('/:petId', async (req, res) => {
   }
 });
 
-// DELETE - Delete Route
+// DELETE - Delete the petId if it exists
 router.delete('/:petId', async (req, res) => {
   try {
     const foundPet = await Pet.findByIdAndDelete(req.params.petId);
@@ -50,6 +50,26 @@ router.delete('/:petId', async (req, res) => {
       throw new Error('Pet not found');
     }
     res.status(200).json(foundPet);
+  } catch (err) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      res.status(500).json({ err: err.message });
+    }
+  }
+});
+
+// PUT - Updates a pet and returns a json response with the updated pet
+router.put('/:petId', async (req, res) => {
+  try {
+    const updatedPet = await Pet.findByIdAndUpdate(req.params.petId, req.body, {
+      new: true,
+    });
+    if (!updatedPet) {
+      res.status(404);
+      throw new Error('pet not found');
+    }
+    res.status(200).json(updatedPet);
   } catch (err) {
     if (res.statusCode === 404) {
       res.json({ err: err.message });
